df7c7363f193fd06c87936b2e1a55d17b584e624
fi00 <259560371@qq.com>
2018-05-04 17:24:16 +0800

    seesion
    seesion

--- src/main/java/cn/coolwind/ssh/web/dao/TestDao.java
+++ src/main/java/cn/coolwind/ssh/web/dao/TestDao.java
@@ -6,17 +6,20 @@ import org.hibernate.query.Query;
 import org.hibernate.transform.Transformers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Transactional
 @Repository
 public class TestDao {
     @Autowired
     private SessionFactory sessionFactory;
 
     public List test() {
-        Session session = sessionFactory.openSession();
+        Session session = sessionFactory.getCurrentSession();
         Query query = session.createSQLQuery("select * from aaa");
-        return query.setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP).list();
+        List list = query.setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP).list();
+        return list;
     }
 }
