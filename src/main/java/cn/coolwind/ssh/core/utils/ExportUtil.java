d873c5fb6bb724e9f80f71dd999a1a1cb048a8e1
fi00 <259560371@qq.com>
2018-05-16 15:24:04 +0800

    .
    .

--- src/main/java/cn/coolwind/ssh/core/utils/ExportUtil.java
+++ src/main/java/cn/coolwind/ssh/core/utils/ExportUtil.java
@@ -7,16 +7,18 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
 import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * 导出工具类
  * @author coolwind
  */
-public class exportUtil {
+public class ExportUtil {
 
     /**
      * 导出
-     * @param list 导入数据
+     * @param list<Object> 导出数据
      * @return HSSFWorkbook
      */
     public static HSSFWorkbook export(List<?> list) {
@@ -37,32 +39,33 @@ public class exportUtil {
                 cell.setCellValue(fields[g].getName());
             }
             //填入数据
-            writeData(1,sheet,list,fields);
+            int dataRow = 1;
+            for (int i = 0; i < list.size(); i++) {
+                HSSFRow row1 = sheet.createRow(dataRow);
+                Object o =  list.get(i);
+                for (int j = 0; j < fields.length; j++) {
+                    HSSFCell cell = row1.createCell(j);
+                    try {
+                        cell.setCellValue((String) fields[j].get(o));
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+                dataRow++;
+            }
 
         }
         return workbook;
     }
 
-    /**
-     * 将list里的数据填入表格
-     * @param startRow 开始的行数
-     * @param sheet     sheet
-     * @param list      数据
-     * @param fields    列名
-     */
-    private static void writeData(int startRow, HSSFSheet sheet, List<?> list, Field[] fields) {
-        for (int i = 0; i < list.size(); i++) {
-            HSSFRow row = sheet.createRow(startRow);
-            Object o =  list.get(i);
-            for (int j = 0; j < fields.length; j++) {
-                HSSFCell cell = row.createCell(j);
-                try {
-                    cell.setCellValue((String) fields[j].get(o));
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                }
-            }
-            startRow++;
+    public static HSSFWorkbook exportListMap(List<Map<String, Object>> list) {
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet sheet = workbook.createSheet();
+        sheet.setColumnWidth(0,40*256);
+        if (list != null && list.size() > 0) {
+            Map<String, Object> map = list.get(0);
+            Set<String> key = map.keySet();
         }
+        return workbook;
     }
 }
